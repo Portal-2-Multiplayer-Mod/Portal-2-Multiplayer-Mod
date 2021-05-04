d4e84dfa0118436b83470e26793a66a48a0b6d75
kyleraykbs <44762123+kyleraykbs@users.noreply.github.com>
2021-05-04 14:45:44 -0700

    Added Windows 7 Warning on The Updater And Updated update.bat

--- MultiplayerModUpdater.cmd
+++ MultiplayerModUpdater.cmd
@@ -1,4 +1,8 @@
 @echo off
+echo Checking OS Version...
+rem detect os version and store it in a variable called "version"
+for /f "tokens=4-5 delims=. " %%i in ('ver') do set VERSION=%%i.%%j
+if "%version%" == "10.0" (cls) else (goto outdatedOS)
 cls
 echo Deleting Old Files...
 del /s MultiplayerModUninstall.cmd
@@ -8,6 +12,7 @@ del /s updatepack.zip
 rmdir /q /s "%cd%\updatepack"
 rmdir /q /s "%cd%\MultiplayerModFiles"
 cls
+echo Check Passed! 
 echo Updating...
 echo Preparing Download...
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/kyleraykbs/Portal2-32PlayerMod-Lite/raw/main/Portal2-32PlayerMod-Lite.Beta.zip', 'updatepack.zip')"
@@ -28,4 +33,11 @@ echo Running Update Specific Code...
 call "%cd%\update.bat"
 echo Code Ran!
 cls
+exit
+:outdatedOS
+color 4
+echo ERROR: Check Failed!!!
+echo ERROR: The updater doesnt support Windows 7 please check https://github.com/kyleraykbs/Portal2-32PlayerMod-Lite/releases/latest for the latest update
+echo Press Any Key
+pause >nul
 exit
