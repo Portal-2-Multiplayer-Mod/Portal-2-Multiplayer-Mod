ec7a7e98d8a969cd0791fcc1420f1a52540b2a06
kyleraykbs <kyleraykbs@gmail.com>
2021-06-26 13:33:17 -0700

    Dedicated Servers Part 6

--- MultiplayerModDedicatedServer/MultiplayerModDedicatedServer.cmd
+++ MultiplayerModDedicatedServer/MultiplayerModDedicatedServer.cmd
@@ -3,4 +3,5 @@ Title Portal 2 Multiplayer Dedicated Server
 cd..
 xcopy /y /S "%cd%\MultiplayerModDedicatedServer\DedicatedServerFiles" "%cd%\portal2"
 cls
-call portal2.exe %windowed% -novid -allowspectators +exec multiplayermod.cfg +sv_lan 0 +mp_wait_for_other_player_notconnecting_timeout 240 +mp_wait_for_other_player_timeout 3 +map mp_coop_lobby_3 -nosixense -textmode -nosound -noipx -nopreload -nojoy -sw +script_execute DedicatedServer -voice_quality 3 +exec DedicatedServer.cfg
+start portal2.exe %windowed% -novid -allowspectators +exec multiplayermod.cfg +sv_lan 0 +mp_wait_for_other_player_notconnecting_timeout 240 +mp_wait_for_other_player_timeout 3 +map mp_coop_lobby_3 -nosixense -textmode -nosound -noipx -nopreload -nojoy -sw +script_execute DedicatedServer -voice_quality 3 +exec DedicatedServer.cfg
+start command.bat
---
+++ MultiplayerModDedicatedServer/command.bat
@@ -0,0 +1,14 @@
+@echo off
+cd..
+:start
+set /P c=Command:
+cls
+echo %c%
+echo %c% > MultiplayerModDedicatedServer\DedicatedServerFiles\cfg\DedicatedServerCommands.cfg
+xcopy /y /S "%cd%\MultiplayerModDedicatedServer\DedicatedServerFiles\cfg\DedicatedServerCommands.cfg" "%cd%\portal2\cfg"
+cls
+timeout 1
+echo. > MultiplayerModDedicatedServer\DedicatedServerFiles\cfg\DedicatedServerCommands.cfg
+xcopy /y /S "%cd%\MultiplayerModDedicatedServer\DedicatedServerFiles\cfg\DedicatedServerCommands.cfg" "%cd%\portal2\cfg"
+cls
+goto start
