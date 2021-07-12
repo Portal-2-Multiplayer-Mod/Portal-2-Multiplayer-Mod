700af704c497f9de9e7ceed0c0e12a7c788acf0e
kyleraykbs <44762123+kyleraykbs@users.noreply.github.com>
2021-07-12 16:53:11 -0700

    Getting Ready For v1.5

--- update.bat
+++ update.bat
@@ -1,7 +1,7 @@
 del /s MultiplayerModUpdater.cmd
+@echo off
 cls
 echo Update Complete!
-echo.
-echo Please Run MultiplayerModGUI.exe
-pause>nul
+echo We're using MultiplayerModGUI.exe in v1.5 now
+timeout 8
 del /s update.bat
