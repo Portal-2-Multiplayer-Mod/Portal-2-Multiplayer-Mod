5dca2d32d00c7dd7d2af97f9388f7d1b71dcf105
kyleraykbs <kyleraykbs@gmail.com>
2021-10-20 15:19:28 -0700

    CreateProps Tool Update

--- MultiplayerModFiles/DevFiles/Tools/CreateProps.lua
+++ MultiplayerModFiles/DevFiles/Tools/CreateProps.lua
@@ -1,3 +1,11 @@
+file.Delete( "GeneratedPortal2ObjectOutput.txt" )
+file.Write( "GeneratedPortal2ObjectOutput.txt", "" )
+
+function GenerateLine(StringDataInput)
+    print(StringDataInput)
+    file.Append("GeneratedPortal2ObjectOutput.txt", "\n" .. StringDataInput )
+end
+
 PropType = ""
 PropCords = Vector (0, 0, 0)
 PropAngles = Vector (0, 0, 0)
@@ -9,7 +17,7 @@ MapName = game.GetMap()
 CachedModels = {}
 ContinueModelCache = true
 
-LineAmount = MapName:len()
+LineAmount = MapName:len() + 19
 Line = ""
 
 while (LineAmount ~= 0) do
@@ -17,27 +25,27 @@ while (LineAmount ~= 0) do
     LineAmount = LineAmount - 1
 end
 
-print("//"..Line.."//")
-print("//" .. MapName .. "//")
-print("//"..Line.."//")
-print("")
+GenerateLine("//"..Line.."//")
+GenerateLine("//CREATE OBJECTS FOR " .. MapName .. "//")
+GenerateLine("//"..Line.."//")
+GenerateLine("")
 
-print('if (GetMapName() == "' .. MapName .. '") {')
+GenerateLine('if (GetMapName() == "' .. MapName .. '") {')
 
 while (Loop == true) do 
 
     if (LoopAmount == 0) then
-        print("    if (CacheTime==true) {")
-        print("        // Cache Objects")
-        print("")
+        GenerateLine("    if (CacheTime==true) {")
+        GenerateLine("        // Cache Objects")
+        GenerateLine("")
     end
 
     if (LoopAmount == 1) then
-        print("")
-        print("")
-        print("    if (CreateTime==true) {")
-        print("        // Create Objects")
-        print("")
+        GenerateLine("")
+        GenerateLine("")
+        GenerateLine("    if (CreateTime==true) {")
+        GenerateLine("        // Create Objects")
+        GenerateLine("")
     end
 
     for k, prop in ipairs( ents.FindByClass( "prop_*" ) ) do
@@ -50,6 +58,11 @@ while (Loop == true) do
             PropAngles = prop:GetAngles()
             PropOutputName = MapName .. "_custom_prop_" .. k
             ContinueModelCache = true
+            PropCollisionNumber = 6
+
+            if (prop:GetCollisionGroup() == 20) then
+                PropCollisionNumber = 0
+            end
 
             -- if the prop has physics store that
             if (prop:GetClass() == "prop_physics" && prop:GetPhysicsObject():IsMotionEnabled()) then
@@ -69,17 +82,17 @@ while (Loop == true) do
                 -- if uncached cache model
                 if (ContinueModelCache == true) then
                     table.insert(CachedModels, PropModel)
-                    print('        CacheModel("' .. PropModel:sub(8) .. '")')
-                    print("")
+                    GenerateLine('        CacheModel("' .. PropModel:sub(8) .. '")')
+                    GenerateLine("")
                 end
             end
             --create code
             if (LoopAmount == 1) then
-                print("        local " .. PropOutputName .. ' = CreateProp("' .. PropType .. '", Vector(' .. PropCords.x .. ", " .. PropCords.y .. ", " .. PropCords.z .. '), "' .. PropModel .. '", 0)')
-                print("        " .. PropOutputName .. ".SetAngles(" .. PropAngles.x .. ", " .. PropAngles.y .. ", " .. PropAngles.z .. ")")
-                print("        " .. PropOutputName .. '.__KeyValueFromString("solid", "6")')
-                print("        " .. PropOutputName .. '.__KeyValueFromString("targetname", "genericcustomprop")')
-                print("")
+                GenerateLine("        local " .. PropOutputName .. ' = CreateProp("' .. PropType .. '", Vector(' .. PropCords.x .. ", " .. PropCords.y .. ", " .. PropCords.z .. '), "' .. PropModel .. '", 0)')
+                GenerateLine("        " .. PropOutputName .. ".SetAngles(" .. PropAngles.x .. ", " .. PropAngles.y .. ", " .. PropAngles.z .. ")")
+                GenerateLine("        " .. PropOutputName .. '.__KeyValueFromString("solid", "'..PropCollisionNumber..'")')
+                GenerateLine("        " .. PropOutputName .. '.__KeyValueFromString("targetname", "genericcustomprop")')
+                GenerateLine("")
             end
 
         end
@@ -87,7 +100,7 @@ while (Loop == true) do
 
     -- add end caching code
     if (LoopAmount == 0) then
-        print("        DoneCacheing <- true")
+        GenerateLine("        DoneCacheing <- true")
     end
 
     -- end if looped twice
@@ -95,12 +108,12 @@ while (Loop == true) do
         Loop = false
     end
 
-    print ("    }")
+    GenerateLine("    }")
 
     LoopAmount = LoopAmount + 1
 end
 
-print("}")
+GenerateLine("}")
 
 -- modelnumber32
 -- modelnumber32.__KeyValueFromString("targetname", "genericcustomprop")
