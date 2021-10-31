68b14d6e78f5f8f7c7c6bc3a2b388f869ea948b3
kyleraykbs <kyleraykbs@gmail.com>
2021-10-31 12:55:03 -0700

    Update MpModFilePathTesting.py

--- MultiplayerModFiles/MainFiles/install_dlc/scripts/vscripts/MpModFilePathTesting.py
+++ MultiplayerModFiles/MainFiles/install_dlc/scripts/vscripts/MpModFilePathTesting.py
@@ -1,4 +1,34 @@
-Dim strSteamInstallPath as String = My.Computer.Registry.GetValue(
-    "HKEY_LOCAL_MACHINE\SOFTWARE\Valve\Steam", "InstallPath", Nothing)
+import os
 
-MsgBox("The install path is " & strSteamInstallPath)
+iow = False
+if os.name == 'nt':
+    print("(System should be running Windows)")
+    iow = True
+else:
+    print("(System should be running Linux)")
+
+CheckingPath = ""
+
+if (iow):
+    print("(System is running Windows)")
+    owd = "C:\Program Files(x86)\Steam\steamapps"
+
+    # open libraryfolders.vdf
+    with open(owd + "\libraryfolders.vdf", "r") as f:
+        s = f.readlines()
+        s = s.replace(" ", "")
+
+        # ittarate through the lines
+        for line in s:
+            print(line)
+            if line.startswith('"path"'):
+                CheckingPath = line[6:]
+                CheckingPath = CheckingPath.replace('"', "")
+                print("VAR SET" + CheckingPath)
+            if (line == '"620""13432591147"'):
+                print("FOUND GAME")
+                FinalPath = CheckingPath + "\\steamapps\\common\\Portal 2"
+                print(FinalPath)
+                break
+
+print("DONE")
