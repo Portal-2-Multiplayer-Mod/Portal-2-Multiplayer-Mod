75693127976ceba477a2c30893203f32416f97a4
kyleraykbs <kyleraykbs@gmail.com>
2021-12-03 11:08:30 -0800

    tempfix

--- MultiplayerModFiles/MultiplayerModLauncher.py
+++ MultiplayerModFiles/MultiplayerModLauncher.py
@@ -918,11 +918,11 @@ def rungui():
                         # Start the game
                         print(SectionConfig("$portal2"))
                         # get current working directory
-                        if (iow):
-                            DownloadFile("https://github.com/kyleraykbs/Portal2-32PlayerMod/archive/refs/heads/main.zip", os.getcwd() + "\\tempinstallmod.zip")
-                        else:
-                            DownloadFile("https://github.com/kyleraykbs/Portal2-32PlayerMod/archive/refs/heads/main.zip", os.getcwd() + "/tempinstallmod.zip")
-                        #LaunchVanillaPortal2(SectionConfig("$portal2"), IsOnProton)
+                        #if (iow):
+                        #    DownloadFile("https://github.com/kyleraykbs/Portal2-32PlayerMod/archive/refs/heads/main.zip", os.getcwd() + "\\tempinstallmod.zip")
+                        #else:
+                        #    DownloadFile("https://github.com/kyleraykbs/Portal2-32PlayerMod/archive/refs/heads/main.zip", os.getcwd() + "/tempinstallmod.zip")
+                        LaunchVanillaPortal2(SectionConfig("$portal2"), IsOnProton)
                     # if proton checkbox is pressed
                     if (iow):
                         continue
