fd911eb0dcd452b6b7094e1a517d06c2e3cdecec
kyleraykbs <44762123+kyleraykbs@users.noreply.github.com>
2022-02-22 14:06:15 -0800

    Create SaveWork.py

---
+++ SaveWork.py
@@ -0,0 +1,9 @@
+import os
+
+#set cur dir to the directory of the script
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+
+# delete everything inside of MultiplayerModFiles/ModFiles/Portal 2/install_dlc
+os.system("rm -rf MultiplayerModFiles/ModFiles/Portal 2/install_dlc/*")
+# copy everything inside of portal2_dlc3/ to MultiplayerModFiles/ModFiles/Portal 2/install_dlc
+os.system("cp -r portal2_dlc3/* MultiplayerModFiles/ModFiles/Portal\ 2/install_dlc/")
