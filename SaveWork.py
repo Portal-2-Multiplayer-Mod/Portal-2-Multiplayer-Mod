bdb4c35cd5cdd487913b8df15258df44872d36bc
kyleraykbs <44762123+kyleraykbs@users.noreply.github.com>
2022-02-24 17:29:34 -0800

    Update SaveWork.py

--- SaveWork.py
+++ SaveWork.py
@@ -3,7 +3,12 @@ import os
 #set cur dir to the directory of the script
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
-# delete everything inside of MultiplayerModFiles/ModFiles/Portal 2/install_dlc
-os.system("rm -rf MultiplayerModFiles/ModFiles/Portal\ 2/install_dlc/*")
-# copy everything inside of portal2_dlc3/ to MultiplayerModFiles/ModFiles/Portal 2/install_dlc
-os.system("cp -r portal2_dlc3/* MultiplayerModFiles/ModFiles/Portal\ 2/install_dlc/")
+# if the folder portal2_dlc3 exists
+if os.path.exists("portal2_dlc3"):
+    # delete everything inside of MultiplayerModFiles/ModFiles/Portal 2/install_dlc
+    os.system("rm -rf MultiplayerModFiles/ModFiles/Portal\ 2/install_dlc/*")
+
+    # copy everything inside of portal2_dlc3/ to MultiplayerModFiles/ModFiles/Portal 2/install_dlc
+    os.system("cp -r portal2_dlc3/* MultiplayerModFiles/ModFiles/Portal\ 2/install_dlc/")
+else:
+    print("portal2_dlc3 folder not found")
