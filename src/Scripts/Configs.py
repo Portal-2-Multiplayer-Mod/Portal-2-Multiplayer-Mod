026115c3f8342eefb4f6aa1d2a79d12d87fea2b7
cabiste <ahmedjj354@gmail.com>
2022-04-18 00:08:20 +0100

    config file will naver be broken again

--- src/Scripts/Configs.py
+++ src/Scripts/Configs.py
@@ -24,61 +24,52 @@ DefaultConfigFile = [
     "#-----------------------------------",
 ]
 
+DefaultConfigFile2 = {
+    "cfgvariant": 18,
+    "portal2path": "undefined",
+    "Test": "false"
+}
+
+def VerifyConfigFileIntegrity(config):
+    if len(config) != len(DefaultConfigFile2) or config.keys() != DefaultConfigFile2.keys():
+        DefaultCopy = DefaultConfigFile2
+        Log("some config data is invalid, fixing it...")
+        for key in DefaultCopy:
+            try:
+                DefaultCopy[key] = config[key]
+            except:
+                Log(f"the key [{key}] is missing from the local config file")
+        Log("writting to file...")
+        WriteConfigFile(DefaultCopy)
+        return DefaultCopy
+        
+    # if the config keys are the same as the default then just return
+    else:
+        return config
+
+def WriteConfigFile(configs):
+    filepath = FindConfigPath()
+    # just to make sure the file doesn't exist
+    try:
+        os.remove(filepath)
+    except:
+        Log("doesn't matter")
+        
+    cfg = open(filepath, "w", encoding="utf-8")
+
+    for key in configs:
+        cfg.write(key+" = "+configs[key] + "\n")
+
+    cfg.close()
 
 def FindConfigPath():
     Log("Finding Config Path...")
-    configpath = GVars.modPath + GVars.nf + "configs.cfg"
-
-    # if it doesn't exist, create it
-    if not os.path.exists(configpath):
-        Log("Config file not found, creating...")
-        Log("")
-        # create the config file
-        configfile = open(configpath, "w", encoding="utf-8")
-        # write the default config file
-        for line in DefaultConfigFile:
-            configfile.write(line + "\n")
-        # close the file
-        configfile.close()
-
-    # return the config path
-    return configpath
-
+    # default config path should be here
+    return GVars.modPath + GVars.nf + "configs.cfg"
 
 def EditConfig(search, newvalue):
-    # gets the config path
-    filepath = FindConfigPath()
-    # opens the config file and read it's content
-    cfgdata = open(filepath, "r", encoding="utf-8").readlines()
-    # delete the file
-    os.remove(filepath)
-    # open the file
-    cfg = open(filepath, "w", encoding="utf-8")
-
-    # go through each line by index so we can see if there is a match
-    for i in range(len(cfgdata)):
-        line = cfgdata[i]
-        # remove everything after the first #
-        line = line.split("#")[0].strip()
-
-        # if the line contains "=" then it's not empty
-        # and if the is longer or equal to 3 characters then then clearly it has some data
-        if ("=" in line and len(line) >= 3):
-            # get everything to the left of the =
-            leftline = line.split("=")[0].strip()
-            # get everything to the right of the =
-            rightline = line.split("=")[1].strip()
-            # if the left side is the search, replace the right side with the new value
-            if (leftline == search):
-                Log("Replacing " + rightline + " with " + newvalue + " in config...")
-                cfgdata[i] = search + " = " + newvalue + "\n"
-
-    # write the file
-    for line in cfgdata:
-        cfg.write(line)
-
-    # close the file
-    cfg.close()
+    GVars.configData[search] = newvalue
+    WriteConfigFile(GVars.configData)
     GVars.LoadConfig()
 
 
@@ -86,9 +77,18 @@ def ImportConfig():
     Log("            __________Config Data Start__________")
     Log("Importing Config...")
 
+    configpath = FindConfigPath()
+    # if the file doesn't exist then create it
+    if not os.path.exists(configpath):
+        WriteConfigFile(DefaultConfigFile2)
+        
     # get the config file and open it
-    config = open(FindConfigPath(), "r", encoding="utf-8").readlines()
+    config = open(configpath, "r", encoding="utf-8").readlines()
 
+    # if the file is empty then also re-create it
+    if len(config) == 0:
+        WriteConfigFile(DefaultConfigFile2)
+        
     # process the config file into useable data
     Log("Processing Config...")
     Log("")
@@ -98,7 +98,7 @@ def ImportConfig():
         line = line.split("#")[0].strip()
 
         # if the line contains "=" then it's not empty 
-        # and if the is longer or equal to 3 characters then then clearly it has some data 
+        # and if the is longer or equal to 3 characters then clearly it has some data 
         if ("=" in line and len(line)>=3):
             # get everything to the left of the = and clean it from whitespaces
             leftline = line.split("=")[0].strip()
@@ -109,8 +109,8 @@ def ImportConfig():
             # if the right line is empty we can handle it later
             if (leftline != ""):
                 processedconfig[leftline] = rightline
-                Log("Line: " + line)
+                Log("Imported: " + line)
 
     Log("")
-    Log("Config Imported!")
-    return processedconfig
+    Log("Configs Imported!")
+    return VerifyConfigFileIntegrity(processedconfig)
