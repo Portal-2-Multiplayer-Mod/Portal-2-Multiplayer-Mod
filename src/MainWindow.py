c5162dd5c6d37023bc325db809d49d881af6f488
kyleraykbs <44762123+kyleraykbs@users.noreply.github.com>
2022-04-19 15:01:43 -0700

    Added Shift Symbol Support

--- src/MainWindow.py
+++ src/MainWindow.py
@@ -527,7 +527,13 @@ def Main():
                             pass
                     elif len(name) == 1:
                         if SHIFTHELD:
-                            CurInput += name.upper()
+                            # if the name doesnt contain a letter
+                            if not name.isalpha():
+                                name = name.replace("1", "!").replace("2", "@").replace("3", "#").replace("4", "$").replace("5", "%").replace("6", "^").replace("7", "&").replace("8", "*").replace("9", "(").replace("0", ")").replace("`", "~").replace("-", "_").replace("=", "+").replace("[", "{").replace("]", "}").replace("\\", "|").replace(";", ":").replace("'", "\"").replace(",", "<").replace(".", ">").replace("/", "?")
+                            # convert lowercase to uppercase
+                            else:
+                                name = name.upper()
+                            CurInput += name
                         else:
                             CurInput += name
             elif event.type == KEYDOWN:
