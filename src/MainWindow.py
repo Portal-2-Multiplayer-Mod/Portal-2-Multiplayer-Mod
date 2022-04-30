d24f345def6f5f2f28206f48dd28623640e91270
kyleraykbs <44762123+kyleraykbs@users.noreply.github.com>
2022-04-30 10:15:40 -0700

    FUCK YOU.pyw

--- src/MainWindow.py
+++ src/MainWindow.py
@@ -67,6 +67,9 @@ pygame.display.set_caption('Portal 2: Multiplayer Mod Launcher')
 
 p2mmlogo = pygame.image.load("GUI/assets/images/p2mm.svg")
 
+# set size to be big
+p2mmlogo = pygame.transform.scale(p2mmlogo, (720, 720))
+
 pygame.display.set_icon(p2mmlogo)
 
 selectedpopupbutton = 0
