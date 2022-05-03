16173dc85f5a7de4f2da1feb0a739e048f92af62
kyleraykbs <44762123+kyleraykbs@users.noreply.github.com>
2022-05-03 16:58:54 -0700

    Update NewSaveWork.py

--- src/NewSaveWork.py
+++ src/NewSaveWork.py
@@ -47,7 +47,7 @@ if os.path.exists(gamepath + "/" + ourfolder):
     else:
         print(os.getcwd())
         # delete everything inside of src/ModFiles/Portal 2/install_dlc
-        os.system( "rmdir /S /Q \"ModFiles\\Portal 2\\install_dlc\"")
+        os.system( "rmdir /S /Q \"src\\ModFiles\\Portal 2\\install_dlc\"")
 
         # copy everything inside of portal2_dlcX/ to src/ModFiles/Portal 2/install_dlc
         shutil.copytree(gamepath + "\\" + ourfolder, "src\\ModFiles\\Portal 2\\install_dlc")
