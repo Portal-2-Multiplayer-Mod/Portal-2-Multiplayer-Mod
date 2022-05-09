992775fcd06bef37b0b86194da0d78e2b1c98210
cabiste <ahmedjj354@gmail.com>
2022-05-09 03:27:29 +0100

    cleaned configs.py

--- src/Scripts/Configs.py
+++ src/Scripts/Configs.py
@@ -1,7 +1,6 @@
 import os
 from Scripts.BasicLogger import Log
 import Scripts.GlobalVariables as GVars
-import ast
 import json
 
 # █▀▀ █▀█ █▄░█ █▀▀ █ █▀▀   █▀▄▀█ ▄▀█ █▄░█ ▄▀█ █▀▀ █▀▀ █▀▄▀█ █▀▀ █▄░█ ▀█▀
@@ -12,82 +11,82 @@ defaultplayerarray = {"name": "New Player", "steamid": "0", "adminlevel": "0"}
 DefaultConfigFile = {
     "cfgvariant":
         {
-            "value" : 18,
-            "menu" : "hidden",
-            "description" : "DO NOT CHANGE THIS NUMBER WILL AUTO-UPDATE",
-            "warning" : "IF YOU CHANGE THIS NUMBER YOU MAY LOSE YOUR CONFIG FILE!",
-            "prompt" : "DONT CHANGE THIS VALUE",
+            "value": 18,
+            "menu": "hidden",
+            "description": "DO NOT CHANGE THIS NUMBER WILL AUTO-UPDATE",
+            "warning": "IF YOU CHANGE THIS NUMBER YOU MAY LOSE YOUR CONFIG FILE!",
+            "prompt": "DONT CHANGE THIS VALUE",
         },
 
     "portal2path":
         {
-            "value" : "undefined",
-            "menu" : "launcher",
-            "description" : "Path to the Portal 2 Folder",
-            "warning" : "",
-            "prompt" : "Enter the path to the Portal 2 folder",
+            "value": "undefined",
+            "menu": "launcher",
+            "description": "Path to the Portal 2 Folder",
+            "warning": "",
+            "prompt": "Enter the path to the Portal 2 folder",
         },
 
     "developer":
         {
-            "value" : "false",
-            "menu" : "launcher",
-            "description" : "Makes the mods files mount from src/ModFiles",
-            "warning" : "",
-            "prompt" : "Enable developer mode?",
+            "value": "false",
+            "menu": "launcher",
+            "description": "Makes the mods files mount from src/ModFiles",
+            "warning": "",
+            "prompt": "Enable developer mode?",
         },
 
     "LauncherSFX":
         {
-            "value" : "true",
-            "menu" : "launcher",
-            "description" : "Makes the buttons play sound effects",
-            "warning" : "",
-            "prompt" : "Enable sound effects?",
+            "value": "true",
+            "menu": "launcher",
+            "description": "Makes the buttons play sound effects",
+            "warning": "",
+            "prompt": "Enable sound effects?",
         },
 
     "LauncherCubes":
         {
-            "value" : "true",
-            "menu" : "launcher",
-            "description" : "Makes cubes rain in the background",
-            "warning" : "",
-            "prompt" : "Enable background cubes?",
+            "value": "true",
+            "menu": "launcher",
+            "description": "Makes cubes rain in the background",
+            "warning": "",
+            "prompt": "Enable background cubes?",
         },
 
     "EncryptCvars":
         {
-            "value" : "false",
-            "menu" : "portal2",
-            "description" : "Encrypts cvars such as \"restart_level\" and \"reset_all_progress\")", 
-            "warning" : "(only use for public games) may break some functionality",
-            "prompt" : "Encrypt cvars?",
+            "value": "false",
+            "menu": "portal2",
+            "description": "Encrypts cvars such as \"restart_level\" and \"reset_all_progress\")",
+            "warning": "(only use for public games) may break some functionality",
+            "prompt": "Encrypt cvars?",
         },
 
     "SafeGuard":
         {
-            "value" : "false",
-            "menu" : "portal2",
-            "description" : "Encrypts vscript functions such as \"SendToConsole(\"\")\"", 
-            "warning" : "(only use for public games) may break some functionality",
-            "prompt" : "Encrypt specific vscript functions?",
+            "value": "false",
+            "menu": "portal2",
+            "description": "Encrypts vscript functions such as \"SendToConsole(\"\")\"",
+            "warning": "(only use for public games) may break some functionality",
+            "prompt": "Encrypt specific vscript functions?",
         },
 
     "Players":
         {
-            "value" : [
+            "value": [
                 defaultplayerarray,
             ],
-            "menu" : "players",
-            "description" : "If You See This Something Is Wrong",
-            "warning" : "If You See This Something Is Wrong",
-            "prompt" : "If you see this something is wrong",
+            "menu": "players",
+            "description": "If You See This Something Is Wrong",
+            "warning": "If You See This Something Is Wrong",
+            "prompt": "If you see this something is wrong",
         },
 }
 
 ImmutableKeys = ["description", "warning", "prompt", "menu"]
 
-# verifies the config file by making sure that the processed data has the same keys as the default 
+# verifies the config file by making sure that the processed data has the same keys as the default
 # if it doesn't then we'll transfer the values from the local config file to the default one and write the default one
 def VerifyConfigFileIntegrity(config):
     # if len(config) != len(DefaultConfigFile) or config.keys() != DefaultConfigFile.keys():
@@ -106,7 +105,7 @@ def VerifyConfigFileIntegrity(config):
     copiedconfig = {
         **config,
     }
-    ########################### VALIDATE ALL THE KEYS ARE CORRECT
+    # VALIDATE ALL THE KEYS ARE CORRECT
     for key1 in copiedconfig:
         if key1 not in DefaultConfigFile:
             Log(f"The key [{key1}] is invalid, fixing it...")
@@ -121,13 +120,13 @@ def VerifyConfigFileIntegrity(config):
                     Log(f"The value for [{key1}][{key2}] is invalid, fixing it...")
                     config[key1][key2] = DefaultConfigFile[key1][key2]
                     WriteConfigFile(config)
-    ########################### VALIDATE ALL THE KEYS EXIST
+    # VALIDATE ALL THE KEYS EXIST
     for key in DefaultConfigFile:
         if key not in config:
             Log(f"The key [{key}] is missing, fixing it...")
             config[key] = DefaultConfigFile[key]
             WriteConfigFile(config)
-    ########################### VALIDATE THAT ALL THE KEYS HAVE ALL THE VALUES
+    # VALIDATE THAT ALL THE KEYS HAVE ALL THE VALUES
     for key in DefaultConfigFile:
         for key2 in DefaultConfigFile[key]:
             if key2 not in config[key]:
@@ -138,6 +137,7 @@ def VerifyConfigFileIntegrity(config):
 
     # if the config keys are the same as the default then just return them
     return config
+
 
 def ValidatePlayerKeys():
     try:
@@ -155,12 +155,14 @@ def ValidatePlayerKeys():
         GVars.configData["Players"]["value"] = [defaultplayerarray]
         WriteConfigFile(GVars.configData)
 
+
 def GetConfigList(search, val):
     lst = []
     for key in GVars.configData:
         if GVars.configData[key][search] == val:
             lst.append(key)
     return lst
+
 
 def WriteConfigFile(configs):
     filepath = FindConfigPath()
@@ -175,17 +177,20 @@ def WriteConfigFile(configs):
     with open(filepath, "w", encoding="utf-8") as cfg:
         json.dump(configs, cfg)
 
+
 # why this is a seperate function that only has 2 lines?
-# well it will make it easier to change the path in the future if we wished to, just change the return value and it will work fine 
+# well it will make it easier to change the path in the future if we wished to, just change the return value and it will work fine
 def FindConfigPath():
     Log("Finding config path...")
     # default config path should be here
     return GVars.configPath + GVars.nf + "configs.cfg"
 
+
 # since we already checked for the integrity of the config file earlier we don't need to re-read from it just change the value in the loaded file and write the whole thing back
 def EditConfig(search, newvalue):
     GVars.configData[search]["value"] = newvalue
     WriteConfigFile(GVars.configData)
+
 
 # to import the config data from the local config file
 def ImportConfig():
@@ -209,21 +214,15 @@ def ImportConfig():
         # process the config file into useable data
         Log("Processing config...")
         Log("")
-        # try:
-        processedconfig = json.loads(config)
-        # except Exception as e:
-        #     Log(f"Error processing config file: {str(e)}")
-        #     WriteConfigFile(DefaultConfigFile)
-        #     processedconfig = json.loads(config)
 
+        processedconfig = json.loads(config)
 
         Log("")
         Log("Configs imported successfully!")
         # at last pass the data to the verify function to make sure everything is clean
         return VerifyConfigFileIntegrity(processedconfig)
     except Exception as e:
-        Log(f"Error importing config: {str(e)}")
-        Log("FAILED TO IMPORT CONFIGS!")
+        Log(f"Error importing the config file: {str(e)}")
         WriteConfigFile(DefaultConfigFile)
         GVars.hadtoresetconfig = True
         return ImportConfig()
