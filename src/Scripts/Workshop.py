b9015a8606b6bc09f9386fc6223dabd21fe9b384
cabiste <ahmedjj354@gmail.com>
2023-07-25 19:12:38 +0100

    added some documentation to 'Scripts/Workshop.py'

--- src/Scripts/Workshop.py
+++ src/Scripts/Workshop.py
@@ -1,14 +1,26 @@
 import os
-from typing import List, Dict
 
 import Scripts.BasicFunctions as BF
 import Scripts.GlobalVariables as GVars
 from Scripts.BasicLogger import Log
 
-# Get a full list of all the users workshop maps so we can find the requested map
-def UpdateMapList(workshoppath: str) -> List[Dict[str, str]]:
-    maplist = []
-    for root, dirs, files in os.walk(workshoppath):
+def GetDownloadedMaps(workshopPath: str) -> set[dict[str, str]]:
+    """returns a list of all downloaded workshop maps
+
+    Parameters
+    ----------
+    workshopPath : str
+        portal 2's workshop folder path
+
+    Returns
+    -------
+    set[dict[str, str]]
+        a list of maps with their 'name', 'bsp' and 'id'
+    """
+
+    mapsList : set[dict[str, str]] = {}
+
+    for root, dirs, files in os.walk(workshopPath):
         if (".jpg" in str(files)) and (".bsp" in str(files)):
             CMap = {
                 "name": "",
@@ -19,19 +31,33 @@ def UpdateMapList(workshoppath: str) -> List[Dict[str, str]]:
             for file in files:
                 if ".bsp" in file:
                     CMap["name"] = file.replace(".bsp", "")
-                    CMap["bsp"] = "workshop" + os.path.join(root, file).replace(workshoppath, "")
+                    CMap["bsp"] = "workshop" + os.path.join(root, file).replace(workshopPath, "")
                 elif ".jpg" in file:
                     CMap["id"] = file.replace("thumb", "").replace(".jpg", "")
-            maplist.append(CMap)
-    return maplist
 
-# Get the workshop pages Steam ID from its link
+            mapsList.add(CMap)
+
+    return mapsList
+
+
 def SteamIDFromLink(link: str) -> str:
+    """Get map id from workshop link
+
+    Parameters
+    ----------
+    link : str
+
+    Returns
+    -------
+    str
+        workshop id
+    """
+
     # Check if it's a valid Steam Workshop link
     if not link.startswith("https://steamcommunity.com/sharedfiles"):
         Log("Not a valid Steam Workshop link!")
         return None
-    
+
     # Find the index of '?id=' in the link
     id_index = link.find('?id=')
 
@@ -48,12 +74,26 @@ def SteamIDFromLink(link: str) -> str:
 
     return link
 
-# Get the changelevel command of a map from the workshop maps Steam Workshop page link
-def MapFromSteamID(workshopLink: str) -> str:
-    SteamID = SteamIDFromLink(workshopLink)
-    maplist = UpdateMapList(GVars.configData["Portal2-Path"]["value"] + BF.NormalizePath("/portal2/maps/workshop"))
 
-    for map_info in maplist:
+def MapFromSteamID(workshopLink: str) -> str:
+    """returns the path for a workshop map from a link
+
+    Parameters
+    ----------
+    workshopLink : str
+        the url of the map
+
+    Returns
+    -------
+    str
+        path relative to the portal 2's path
+    """
+
+    SteamID = SteamIDFromLink(workshopLink)
+    mapsList = GetDownloadedMaps(GVars.configData["Portal2-Path"]["value"] + BF.NormalizePath("/portal2/maps/workshop"))
+
+    for map_info in mapsList:
         if map_info["id"] == SteamID:
             return map_info["bsp"].replace(".bsp", "")
+
     return None
