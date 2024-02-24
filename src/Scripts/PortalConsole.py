5c57262a58077edf0b13485075a6eed92e21d84b
cabiste <ahmedjj354@gmail.com>
2024-02-24 12:21:14 +0100

    added a filter when logging the portal 2 console

--- src/Scripts/PortalConsole.py
+++ src/Scripts/PortalConsole.py
@@ -20,17 +20,38 @@ def WriteConsoleToLog() -> None:
     consoleFile = GetConsoleFile()
     if consoleFile == None:
         return
-    
+
     logFile = GetLastLog()
-    
+
+    # just using the logger might be better looking, but i just prefer this monstrosity
     with open(logFile, "a") as log:
         log.write("# PORTAL 2 CONSOLE LOGS:\n\n")
-        with open(consoleFile, "r") as console:
-            log.write(FilterConsole(console.read()))
+        log.writelines(
+            FilterConsole(open(consoleFile, "r").readlines())
+        )
         log.write("\n\n# PORTAL 2 CONSOLE LOGS END.\n\n")
-    
+
     # to avoid duplicates
     BF.DeleteFile(consoleFile)
 
-def FilterConsole(text:str) -> str:
+def FilterConsole(text: list[str]) -> list[str]:
+    # removes the lines related to initializing the game
+    text = text[1200:]
+
+    i = -1
+    while True:
+        i +=1
+        if i == len(text)-1: break
+
+        # removes duplicate lines (idk why but portal logs everything twice when loading)
+        if text[i] == text[i+1]:
+            text.pop(i)
+            i -= 1
+            continue
+
+        # masks the user's ip (ik it's cursed but i ran some benchmarks and it's fast - cabiste)
+        if "IP " in text[i]:
+            tex[i] += " "
+            tex[i] = tex[i].replace(tex[i][tex[i].index("IP ")+3: tex[i].index(" ", tex[i].index("IP ")+3, len(tex[i]))], "XXX.XXX.XXX.XXX")
+
     return text
